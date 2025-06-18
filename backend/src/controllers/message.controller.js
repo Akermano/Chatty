51dd82d7855c1dcc77ccb21ee33cbbe73b8a09af
Akermano <96015205+Akermano@users.noreply.github.com>
2025-06-18 19:49:59 +0300

    Update message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,6 +2,19 @@ import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 import { getReceiverSocketId, io } from "../lib/socket.js";
 
+// 🔹 Sidebar: List all users except self
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error in getUsersForSidebar:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// 🔹 Chat: Get all messages between two users
 export const getMessages = async (req, res) => {
   try {
     const { id: userToChatId } = req.params;
@@ -21,6 +34,7 @@ export const getMessages = async (req, res) => {
   }
 };
 
+// 🔹 Chat: Save encrypted message/image
 export const sendMessage = async (req, res) => {
   try {
     const { text, image, imageKey, iv } = req.body;
@@ -30,10 +44,10 @@ export const sendMessage = async (req, res) => {
     const newMessage = new Message({
       senderId,
       receiverId,
-      text,
-      image,
-      imageKey,
-      iv,
+      text,       // encrypted text (RSA)
+      image,      // encrypted image (AES base64)
+      imageKey,   // encrypted AES key (RSA)
+      iv          // AES IV (base64)
     });
 
     await newMessage.save();
